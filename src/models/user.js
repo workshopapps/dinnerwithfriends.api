e43839fe116edec0692879d2d68156194364eca7
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-11-17 19:17:23 +0100

    Add user model
    Signed-off-by: Ekene Nwobodo <nwobodoe71@gmail.com>

---
+++ src/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required:[true,"A user must have a name"]
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: [true, 'A user must have an email'],
+      lowercase: true,
+    },
+    password: {
+        type: String,
+        required: [true, 'Enter a user password'],
+        minLength: 8,
+        select: false,
+    },
+    refreshToken:{
+        type:String,
+        trim:true,
+        default:null
+    },
+    verifiedEmail: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.password) next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
