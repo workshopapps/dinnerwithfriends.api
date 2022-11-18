2f9ee9850367d6220f29d60a7421ca69df388cb4
Edidiong Udoh <gisthive@gmail.com>
2022-11-18 20:28:29 +0100

    BAC-1

--- src/routes/v1/event.js
+++ src/routes/v1/event.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', eventControllers.getAllEvents);
 router.post('/add', eventControllers.addEvent);
 router.get('/single/:event_id', eventControllers.getSingleEvent);
-// router.delete('/delete/:event_id', eventControllers.deleteEvent);
+router.delete('/delete/:event_id', eventControllers.deleteEvent);
 router.patch('/update/:event_id', eventControllers.updateEvent);
 
 module.exports = router;
