6902ec3c98f853fed59552778434f714f5e8f0b5
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2022-11-19 00:26:46 +0100

    bac-19-database-model-create-participant-model

---
+++ src/models/participant.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const participantSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: [3, `fullname can't be less than 3 characters`]
+    },
+
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: [true, `email field must be specified !!!`],
+        lowercase: true,
+      },
+
+      prefered_date_time: {
+        type: String,
+        required: true,
+        trim: true
+      }
+
+})
+
+const Participant = mongoose.model("Participant", participantSchema);
+module.exports = Participant;
