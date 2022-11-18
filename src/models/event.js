0d4d67f7efe1f3d6621c008b01a7cf03cdfa60ca
Jeremiah-Ropo <patrickjerry59@yahoo.com>
2022-11-19 00:57:07 +0100

    bac-18-database-model-create-event-model

--- src/models/event.js
+++ src/models/event.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 const eventSchema = new mongoose.Schema({
+
+    // user_id: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "User",
+    //     required: true
+    // },
     event_title: {
         type: String,
         required: true,
