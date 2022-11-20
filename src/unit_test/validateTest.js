3ee5b20813c72aa6f1da69f67e46dd7a36d0a5e9
Promise <promiseprince01@gmail.com>
2022-11-20 07:09:36 +0100

    added unit testing for login

--- src/unit_test/validateTest.js
+++ src/unit_test/validateTest.js
@@ -1,12 +1,27 @@
 const validateUser = require('../validators/createUser');
+const loginUser = require('../validators/loginUser');
 
-// Testing the user validator
-test('checking the user validation', () => {
-  validateUser = mockRequest({
-    name: "prince",
-    email: "string",
-    password: "string"
-});
 
-expect(validateUser).toMatch(mockRequest);
+
+describe('First Group Of Tests', () => {
+  // Testing the user validator
+  it('checking the user validation', () => {
+    validateUser = mockRequest({
+      name: "string",
+      email: "string",
+      password: "string"
+  });
+  expect(validateUser).toMatch(mockRequest);
+  })
+
+  // Testing the login schema 
+  it('checking login schema', () => {
+    loginUser = mockRequest({
+      email: 'string',
+      password: 'string'
+    })
+    expected(loginUser).toMatch(mockRequest);
+  })
+
+  
 })
