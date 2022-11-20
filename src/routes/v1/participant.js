c99cd1a21fcb763488f73c4f53dff499c085f5e1
Angelo-Ufelle <onyedikaufelle@gmail.com>
2022-11-20 15:37:41 +0100

    added update and delete participant

--- src/routes/v1/participant.js
+++ src/routes/v1/participant.js
@@ -1,18 +1,15 @@
 /* eslint-disable linebreak-style */
 const express = require('express');
 const { participantControllers } = require('../../controllers');
-const services = require('../../services');
 
 const router = express.Router();
-
-// router.use(services.protect);
 
 router.post('/addpart', participantControllers.addParticipant);
 
 // delete user
-router.delete('/delete', participantControllers.deleteParticipant);
+router.delete('/delete/:id', participantControllers.deleteParticipant);
 
 // update participant
-router.put('/updatepart', participantControllers.updateParticipant);
+router.post('/update/:id', participantControllers.updateParticipant);
 
 module.exports = router;
