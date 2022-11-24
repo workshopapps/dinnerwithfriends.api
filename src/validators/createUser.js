5c19d24ed3428eba6ab8ab66bede46cb0621d956
Nwobodo Leonard <nwobodoe71@gmail.com>
2022-11-24 20:51:54 +0100

    Revert "Make user name optional"

--- src/validators/createUser.js
+++ src/validators/createUser.js
@@ -4,7 +4,8 @@ const createUserSchema = Joi.object({
   name: Joi.string()
     .pattern(/^[A-Za-z][A-Za-z ]{2,30}$/)
     .min(3)
-    .max(30),
+    .max(30)
+    .required(),
 
   email: Joi.string()
     .email({ tlds: { allow: false } })
