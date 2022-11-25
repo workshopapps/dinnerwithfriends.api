53864de40c45e854998c045917cfdbc47761b73d
Nwobodo Leonard <nwobodoe71@gmail.com>
2022-11-25 08:08:54 +0100

    Revert "Bac 23 invitation crud"

--- src/routes/v1/invitation.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createInvite } = require('../../controllers/invite');
-const services = require('../../services');
-
-const router = express.Router();
-router.use(services.protect);
-
-router.get('/invite', createInvite);
