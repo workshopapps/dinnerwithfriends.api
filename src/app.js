a12f6c7c7195bc20e957a55c60f30c31b99b9a41
Precious Adedibu <adedibuprecious@gmail.com>
2022-11-30 17:41:50 +0100

    fixed cors

--- src/app.js
+++ src/app.js
@@ -10,27 +10,32 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocumentation = require('./utilities/documentation');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
-const cron = require("node-cron");
-const { generateFinalEventsDates } = require('./services/generateFinalEventDate');
-
+const cron = require('node-cron');
+const {
+  generateFinalEventsDates,
+} = require('./services/generateFinalEventDate');
+const corsOptions = require('./config/corsOptions');
 
 // create an express app
 const app = express();
 
-var corsOptions = {
-  origin: 'https://catchup.hng.tech',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+// var corsOptions = {
+//   origin: 'https://catchup.hng.tech',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
 
 cron.schedule('0 1 * * *', async () => {
-    console.log('Running a task every midnight (1:00 am)');
-    await generateFinalEventsDates()
-  });
+  console.log('Running a task every midnight (1:00 am)');
+  await generateFinalEventsDates();
+});
 
 // middlewares
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://catchup.hng.tech"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://catchup.hng.tech'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 app.use('/api-docs', swaggerUi.serve);
