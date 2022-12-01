d446d8201cd4ed7a579cc8f8ad1164cb1a61a73a
Precious Adedibu <adedibuprecious@gmail.com>
2022-12-01 19:40:05 +0100

    google signin and sign up done

--- src/services/index.js
+++ src/services/index.js
@@ -1,19 +1,27 @@
-const {createSendToken, protect, googleSendToken,signRefreshToken, createSendData} = require("./auth")
-const {newParticipantToken, protectParticipant} = require("./participant")
-const {newEventToken, protectEvent} = require("./event")
-const generateFinalEventDate = require("./generateFinalEventDate")
-const jwt = require("./jwt.services")
+const {
+  createSendToken,
+  protect,
+  googleSendToken,
+  signRefreshToken,
+  createSendData,
+  getTokens,
+} = require('./auth');
+const { newParticipantToken, protectParticipant } = require('./participant');
+const { newEventToken, protectEvent } = require('./event');
+const generateFinalEventDate = require('./generateFinalEventDate');
+const jwt = require('./jwt.services');
 
 module.exports = {
-    createSendToken,
-    protect,
-    jwt,
-    signRefreshToken,
-    newParticipantToken,
-    protectParticipant,
-    newEventToken,
-    protectEvent,
-    googleSendToken,
-    generateFinalEventDate,
-    createSendData
-}
+  getTokens,
+  createSendToken,
+  protect,
+  jwt,
+  signRefreshToken,
+  newParticipantToken,
+  protectParticipant,
+  newEventToken,
+  protectEvent,
+  googleSendToken,
+  generateFinalEventDate,
+  createSendData,
+};
