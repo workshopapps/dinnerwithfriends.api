0c696cdde3a0f172d8aa0bb43b2be920e48a479d
Edidiong Udoh <42146638+technicaldee@users.noreply.github.com>
2022-12-02 10:50:26 +0100

    Update allowedOrigins.js

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,13 +1,14 @@
 const allowedOrigins = [
-  'https://catchup.hng.tech',
-  'https://https://prybar.vercel.app',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'http://localhost:3001',
-  'http://127.0.0.1:3001',
-  'http://localhost:3002',
-  'http://127.0.0.1:3002',
-  'https://dinnerwithfriends-web-rho.vercel.app/',
+  '*',
+//   'https://catchup.hng.tech',
+//   'https://https://prybar.vercel.app',
+//   'http://localhost:3000',
+//   'http://127.0.0.1:3000',
+//   'http://localhost:3001',
+//   'http://127.0.0.1:3001',
+//   'http://localhost:3002',
+//   'http://127.0.0.1:3002',
+//   'https://dinnerwithfriends-web-rho.vercel.app/',
 ];
 
 module.exports = allowedOrigins;
