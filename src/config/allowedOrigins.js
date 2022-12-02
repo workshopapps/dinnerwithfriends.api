8dffaff86fcec4fbdb507e6796faddc3ab301cfe
Precious Adedibu <adedibuprecious@gmail.com>
2022-12-02 10:44:24 +0100

    cors

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -1,12 +1,13 @@
 const allowedOrigins = [
-  'https://catchup.hng.tech',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'http://localhost:3001',
-  'http://127.0.0.1:3001',
-  'http://localhost:3002',
-  'http://127.0.0.1:3002',
-  'https://dinnerwithfriends-web-rho.vercel.app/',
+  '*',
+  // 'https://catchup.hng.tech',
+  // 'http://localhost:3000',
+  // 'http://127.0.0.1:3000',
+  // 'http://localhost:3001',
+  // 'http://127.0.0.1:3001',
+  // 'http://localhost:3002',
+  // 'http://127.0.0.1:3002',
+  // 'https://dinnerwithfriends-web-rho.vercel.app/',
 ];
 
 module.exports = allowedOrigins;
