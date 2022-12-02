cfe84be07df464c2d28d23063e1a01d7c0014260
Precious Adedibu <adedibuprecious@gmail.com>
2022-12-02 10:38:09 +0100

    added new cors site

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -6,6 +6,7 @@ const allowedOrigins = [
   'http://127.0.0.1:3001',
   'http://localhost:3002',
   'http://127.0.0.1:3002',
+  'https://dinnerwithfriends-web-rho.vercel.app/',
 ];
 
 module.exports = allowedOrigins;
