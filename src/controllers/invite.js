14ec2289e5765b0486d6601992a4c5e634657b05
Precious Adedibu <adedibuprecious@gmail.com>
2022-12-03 02:18:51 +0100

    updating invite crud

--- src/controllers/invite.js
+++ src/controllers/invite.js
@@ -15,84 +15,93 @@ module.exports.createInvite = asyncHandler(async (req, res, next) => {
   //     next(new AppError('Event not found', 404));
   //   }
   // }
+
+  const foundEvent = await Event.findOne({ _id: event_id });
+  if (foundEvent.user_id.toString() !== req.user._id.toString()) {
+    next(new AppError('You are not the host of this event', 401));
+  }
+
   let memo = [];
   for (let i = 0; i < email_list.length; i++) {
     if (memo.includes(email_list[i].toLowerCase()) === false) {
       const eventToken = await generateJWTToken(
-        event_id,
+        { event_id },
         process.env.INVITATION_TOKEN_SECRET,
-        '1d'
+        '90d'
       );
-
       const invitationLink =
         'https://catchup.hng.tech/participants/' + eventToken;
-      const email = email_list[i];
-      await sendInvitationLink(invitationLink, email);
-      memo.push(email_list[i].toLowerCase());
+
+      //find invitation where email and event_id match
+      const foundInvitation = await Invitation.findOne({
+        email: email_list[i].toLowerCase(),
+        event_id,
+      });
+
+      if (!foundInvitation) {
+        const email = email_list[i];
+        //      await sendInvitationLink(invitationLink, email);
+        memo.push(email_list[i].toLowerCase());
+        const invitationPayload = {
+          email: email_list[i],
+          event_id,
+        };
+
+        const newInvitation = await new Invitation(invitationPayload).save();
+      }
     }
   }
 
-  const invitationPayload = {
-    email_list: memo,
-    event_id,
-    userId: req.user.id,
-    active: true,
-  };
-
-  const newInvitation = await new Invitation(invitationPayload).save();
-
-  return res.send({
+  return res.json({
     status: 'success',
-    message: 'Invitations have been sent successfully',
-    data: {
-      newInvitation: newInvitation,
-    },
+    message: 'Invitations have been processed',
   });
 });
 
-module.exports.updateInvite = asyncHandler(async (req, res, next) => {
-  const { email_list, event_id } = req.body;
-  const { id } = req.params;
+// module.exports.updateInvite = asyncHandler(async (req, res, next) => {
+//   const { email_list, event_id } = req.body;
+//   const { id } = req.params;
 
-  const newInvitation = await Invitation.findById(id);
-  if (!newInvitation) {
-    next(new AppError('Invitation not found', 404));
-  }
+//   const newInvitation = await Invitation.findById(id);
+//   if (!newInvitation) {
+//     next(new AppError('Invitation not found', 404));
+//   }
 
-  let memo = [];
-  for (let i = 0; i < email_list.length; i++) {
-    if (
-      memo.includes(email_list[i].toLowerCase()) === false &&
-      newInvitation.email_list.includes(email_list[i].toLowerCase()) === false
-    ) {
-      const eventToken = await generateJWTToken(
-        event_id,
-        process.env.INVITATION_TOKEN_SECRET,
-        '1d'
-      );
+//   let memo = [];
+//   for (let i = 0; i < email_list.length; i++) {
+//     if (
+//       memo.includes(email_list[i].toLowerCase()) === false &&
+//       newInvitation.email_list.includes(email_list[i].toLowerCase()) === false
+//     ) {
+//       const eventToken = await generateJWTToken(
+//         event_id,
+//         process.env.INVITATION_TOKEN_SECRET,
+//         '1d'
+//       );
 
-      const invitationLink =
-        'https://catchup.hng.tech/participants/' + eventToken;
-      const email = email_list[i];
-      memo.push(email_list[i].toLowerCase());
-      await sendInvitationLink(invitationLink, email);
-    }
-  }
+//       const invitationLink =
+//         'https://catchup.hng.tech/participants/' + eventToken;
+//       const email = email_list[i];
+//       memo.push(email_list[i].toLowerCase());
+//       await sendInvitationLink(invitationLink, email);
+//     }
+//   }
 
-  newInvitation.email_list = [...newInvitation.email_list, ...memo];
-  newInvitation.save();
-  return res.send({
-    status: 'success',
-    message: 'Invitations have been updated and send successfully',
-    data: {
-      newInvitation: newInvitation,
-    },
-  });
-});
+//   newInvitation.email_list = [...newInvitation.email_list, ...memo];
+//   newInvitation.save();
+//   return res.send({
+//     status: 'success',
+//     message: 'Invitations have been updated and send successfully',
+//     data: {
+//       newInvitation: newInvitation,
+//     },
+//   });
+// });
 
 module.exports.deleteInvite = asyncHandler(async (req, res, next) => {
   const { email, event_id } = req.body;
   const { id } = req.params;
+  // await Invitation.deleteMany({});
   const newInvitation = await Invitation.findByIdAndUpdate(id, {
     $pull: { email_list: email },
   });
@@ -122,4 +131,19 @@ module.exports.getEventDetails = asyncHandler(async (req, res, next) => {
   const { jwt } = req.params;
   const payload = await jwt.verify(jwt, process.env.INVITATION_TOKEN_SECRET);
   return res.status(200).json({ payload });
+});
+
+
+module.exports.getEventInvites = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const invites = await Invitation.find({ event_id: id }).sort({
+    createdAt: 'desc',
+  });
+  return res.send({
+    status: 'success',
+    message: 'Invites retrieved successfully',
+    data: {
+      invites,
+    },
+  });
 });
--- src/routes/v1/invitation.js
+++ src/routes/v1/invitation.js
@@ -5,6 +5,7 @@ const {
   deleteInvite,
   getAllInvites,
   getEventDetails,
+  getEventInvites,
 } = require('../../controllers/invite');
 const services = require('../../services');
 
@@ -13,7 +14,9 @@ router.use(services.protect);
 
 router.get('/', getAllInvites);
 router.post('/', createInvite);
-router.patch('/:id', updateInvite);
+// router.patch('/:id', updateInvite);
 router.delete('/:id', deleteInvite);
 router.get("/event/:jwt", getEventDetails);
+router.get('/event/:id', getEventInvites);
+
 module.exports = router;
