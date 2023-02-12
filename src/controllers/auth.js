190ec8442f21c51bd5fe5fbde3ca272a79c9462e
Preshy-Jones <adedibuprecious@gmail.com>
2023-02-12 15:30:30 +0100

    attempt2

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -272,13 +272,13 @@ const googleAuthRedirect = asyncHandler(async (req, res, next) => {
   res.cookie('accessToken', accessToken, {
     httpOnly: false,
     // sameSite: 'none',
-    domain: 'catchup.rsvp',
+    domain: '*',
     maxAge: 24 * 60 * 60 * 1000,
   });
   res.cookie('refreshToken', refreshToken, {
     httpOnly: false,
     // sameSite: 'none',
-    domain: 'catchup.rsvp',
+    domain: '*',
     maxAge: 24 * 60 * 60 * 1000,
   });
   res.redirect(process.env.UI_ROOT_URI);
